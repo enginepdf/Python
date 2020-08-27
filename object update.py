4097cc0dff3b0d8c41001bb90c828d16968adf30
engineve <yeindap@gmail.com>
2020-08-27 11:11:04 +0900

    Update object update.py
    dict method

--- object update.py
+++ object update.py
@@ -21,3 +21,10 @@ print(myitems) # {12345: 'number', 'car': None, 'driver': 'me', 'snacks': ['prin
 print(items) # {12345: 'number', 'car': 'BMW', 'driver': 'Ed Sheeran', 'snacks': ['pringles', 'cola'], 'is_on': True}
 print(myitems.get('money')) # None
 print(myitems.get('money', 10000)) # 10000
+
+newItems=dict(temp='number', car= 'Hyundai',
+    driver= 'me',
+    snacks= ['pringles', 'cola'],
+    is_on= True)
+
+print(newItems) # {'temp': 'number', 'car': 'Hyundai', 'driver': 'me', 'snacks': ['pringles', 'cola'], 'is_on': True}
