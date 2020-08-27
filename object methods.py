2318ff94f5b2c22537ce3e58ea96984cc619c6e1
engineve <yeindap@gmail.com>
2020-08-27 11:24:34 +0900

    Changed object update.py to object methods.py
    added some more object methods

--- object methods.py
+++ object methods.py
@@ -8,6 +8,8 @@ items = {
 }
 
 print(items.keys()) # dict_keys([12345, 'car', 'driver', 'snacks', 'is_on'])
+print(items.values()) # dict_values(['number', 'BMW', 'Ed Sheeran', ['pringles', 'cola'], True])
+print(items.items()) # dict_items([(12345, 'number'), ('car', 'BMW'), ('driver', 'Ed Sheeran'), ('snacks', ['pringles', 'cola']), ('is_on', True)])
 
 items.update({'is_on': False}) 
 print(items) # {12345: 'number', 'car': 'BMW', 'driver': 'Ed Sheeran', 'snacks': ['pringles', 'cola'], 'is_on': False}
@@ -28,3 +30,9 @@ newItems=dict(temp='number', car= 'Hyundai',
     is_on= True)
 
 print(newItems) # {'temp': 'number', 'car': 'Hyundai', 'driver': 'me', 'snacks': ['pringles', 'cola'], 'is_on': True}
+
+print(newItems['snacks'].pop()) # cola
+print(newItems.popitem()) # ('is_on', True)  not always last item
+print(newItems.pop('temp')) # number
+print(newItems.clear()) # None
+print(newItems) # {}
