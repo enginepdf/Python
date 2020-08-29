e59e4a681bc89c47619cacca3d1dcc7cd2449257
engineve <yeindap@gmail.com>
2020-08-29 15:16:23 +0900

    Create elif classics.py
    elif if else statements

---
+++ elif classics.py
@@ -0,0 +1,15 @@
+money=15000
+
+if money<10000 and money>=5000:
+    print('Your seat is R')
+
+elif money>=10000 and money<20000:
+    print('Your seat is S') # Your seat is S
+
+elif money>=20000:
+    print('Your seat is VIP')
+
+else:
+    print('Study Hard, Work Hard')
+
+print('Enjoy the classics') # Enjoy the classics
