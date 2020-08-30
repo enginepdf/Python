4adae8c9f1cca3cac00dfb258dfd7bbbf8d3d8f2
engineve <yeindap@gmail.com>
2020-08-30 14:42:10 +0900

    Create isOp.py
    is comparison with logical operator

---
+++ isOp.py
@@ -0,0 +1,9 @@
+print(True == 1) # True  bool(1)
+print(100 == 100.0) # True
+print([1,2] == [1,2]) # True
+print('100'==100) # False
+
+print(True is 1) # False
+print(100 is 100.0) # False
+print([1,2] is [1,2]) # False
+print(True is True) # True
