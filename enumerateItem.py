8e8a7ea8800354329a30989c250b2520c01b9d62
engineve <yeindap@gmail.com>
2020-08-31 09:59:31 +0900

    Update enumerateItem.py
    some more examples

--- enumerateItem.py
+++ enumerateItem.py
@@ -18,3 +18,22 @@ for idx, val in enumerate(data):
 # 7 7
 # 8 8
 # 9 9
+
+for idx, val in enumerate('Enumerate'):
+    print(idx, val)
+
+# 0 E
+# 1 n
+# 2 u
+# 3 m
+# 4 e
+# 5 r
+# 6 a
+# 7 t
+# 8 e
+
+for idx, val in enumerate({1,2,3}):
+    print(idx, val)
+# 0 1
+# 1 2
+# 2 3
