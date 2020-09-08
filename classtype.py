ea7724c379548b60bd6277a234261adf3d304bbb
engineve <yeindap@gmail.com>
2020-09-08 16:45:55 +0900

    Update classtype.py
    dunder method __init__
    another class added

--- classtype.py
+++ classtype.py
@@ -4,3 +4,17 @@ class newobj:
 item=newobj()
 
 print(type(item)) # <class '__main__.newobj'>
+
+
+class items:
+    def __init__(self, name):
+        self.name=name
+    
+    def display(self):
+        print(self.name)
+
+item=items('earphone')
+item.price=100
+print(item.name) # earphone
+item.display() # earphone
+print(item.price) # 100
