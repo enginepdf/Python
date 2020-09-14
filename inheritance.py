eb5a1bfc55152064bb7c7efaebf2b517fc3dbd34
engineve <yeindap@gmail.com>
2020-09-14 12:00:37 +0900

    Update inheritance.py
    multi inheritance

--- inheritance.py
+++ inheritance.py
@@ -8,6 +8,23 @@ class Apple(State):
         State.__init__(self, True) # super().__init__(True)
         self.name = name
         self.price = price
+    
+    def displayA(self):
+        print(f'this is {self.name}')
+
+class Samsung(State):
+    def __init__(self, name, price):
+        State.__init__(self, True) # super().__init__(True)
+        self.name = name
+        self.price = price
+    
+    def displayS(self):
+        print(f'this is {self.name}')
+
+class Product(Apple, Samsung):
+    def __init__(self, name1,price1,name2,price2):
+        Apple.__init__(self,name1,price1)
+        Samsung.__init__(self,name2,price2)
 
 
 Mac = Apple("Macbook", 2000)
@@ -31,3 +48,7 @@ print(f"{most_expensive(Mac, Iphone, Ipad)} is the most expensive one.")  # Macb
 print(isinstance(Mac, Apple)) # True
 print(isinstance(Mac, State)) # True
 print(Mac.new) # True
+
+product=Product('Iphone',500,'Galaxy',500)
+
+product.displayS() # this is Galaxy
