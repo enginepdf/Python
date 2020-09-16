b49f610da0b744c15dd8f5921b4d13a138ee2fd1
engineve <yeindap@gmail.com>
2020-09-16 15:34:18 +0900

    Update lambda.py
    sorting with lambda

--- lambda.py
+++ lambda.py
@@ -1,4 +1,9 @@
 items=[10,20,30]
 
+foods=[('chicken',1,5000),('ham',10,50000),('pizza',3,30000)]
+
+
 print(list(map(lambda item: item**3, items))) # [1000, 8000, 27000]
 print(list(filter(lambda item: item%10 ==0, items))) # [10, 20, 30]
+foods.sort(key=lambda x:x[2])
+print(foods) # [('chicken', 1, 5000), ('pizza', 3, 30000), ('ham', 10, 50000)]
