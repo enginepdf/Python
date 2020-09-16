e3bda6feeeca0a113b9f01040d6c8ab73e7fd97f
engineve <yeindap@gmail.com>
2020-09-16 15:52:07 +0900

    Update lambda.py
    lambda using multiply

--- lambda.py
+++ lambda.py
@@ -1,9 +1,9 @@
 items=[10,20,30]
 
-foods=[('chicken',1,5000),('ham',10,50000),('pizza',3,30000)]
+foods=[('chicken',2,10000),('ham',10,5000),('pizza',3,10000)] # (name, quantitiy, price per unit)
 
 
 print(list(map(lambda item: item**3, items))) # [1000, 8000, 27000]
 print(list(filter(lambda item: item%10 ==0, items))) # [10, 20, 30]
-foods.sort(key=lambda x:x[2], reverse=True)
-print(foods) # [('ham', 10, 50000), ('pizza', 3, 30000), ('chicken', 1, 5000)]
+foods.sort(key=lambda x:x[1]*x[2], reverse=True)
+print(foods) # [('ham', 10, 5000), ('pizza', 3, 10000), ('chicken', 2, 10000)]
