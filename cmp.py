af9b6a48ce67c8f763a9cc85932478fbf03a455f
engineve <yeindap@gmail.com>
2020-09-18 21:59:02 +0900

    Create cmp.py
    cmp function

---
+++ cmp.py
@@ -0,0 +1,18 @@
+def cmp(a, b):
+    return (a > b) - (a < b) 
+
+a = 100
+b = 150
+print(cmp(a, b)) # -1
+  
+a = 1
+b = 1
+print(cmp(a, b)) # 0
+   
+a = 150
+b = 100
+print(cmp(a, b)) # 1
+
+print(100>150) # False
+print(100<150) # True
+print((100>150)-(100<150)) # -1
