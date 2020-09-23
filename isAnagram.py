d602eeb318b8a700a3e1a51a0d87162b4ad8c60d
engineve <yeindap@gmail.com>
2020-09-23 19:23:41 +0900

    Create isAnagram.py
    anagram checking

---
+++ isAnagram.py
@@ -0,0 +1,24 @@
+# Reference : https://stackoverflow.com/questions/8286554/using-python-find-anagrams-for-a-list-of-words
+# https://en.wikipedia.org/wiki/Anagram
+
+import re 
+worker = re.compile(r'\s+') 
+
+def isAnagram(str1, str2):
+    str1_list = list(worker.sub('',str1).lower())
+    str1_list.sort()
+    print(str1_list)
+    str2_list = list(worker.sub('',str2).lower())
+    str2_list.sort()
+    print(str2_list)
+
+    return (str1_list == str2_list)
+
+str1="McDonald's restaurants" 
+str2="Uncle Sam's standard rot"
+
+print(isAnagram(str1, str2))
+
+
+
+
