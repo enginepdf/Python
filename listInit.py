7aafb7d95e4c9d1210c83ab7ef976f219d0501cf
engineve <yeindap@gmail.com>
2020-10-06 00:23:47 +0900

    Update listInit.py
    matrix using list

--- listInit.py
+++ listInit.py
@@ -2,3 +2,11 @@ item=['10 dollar']*10
 
 print(item)
 # ['10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar', '10 dollar']
+
+arr=[[0]*5 for _ in range(5)]
+print(arr)
+# [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+arr1=[[0]*5]*5
+print(arr1)
+# [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
