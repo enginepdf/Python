e59f111c59ff57a43c1f5f9e5633f1b910d142e9
engineve <yeindap@gmail.com>
2020-10-06 10:48:00 +0900

    Update exponential.py
    import time, time.time()

--- exponential.py
+++ exponential.py
@@ -1,3 +1,6 @@
+import time
+start=time.time()
+
 num=1e9
 
 num1=100.001e1
@@ -8,3 +11,5 @@ print(num) # 1000000000.0
 print(num1) # 1000.01
 print(num2) # 0.00100001
 print(round(num2,5)) # 0.001
+end=time.time()
+print('time:', end-start) # time: 7.200241088867188e-05
