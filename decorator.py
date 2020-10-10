8440e4e7bfd0c58b6bcb86907eb9d3d77bc7e630
engineve <yeindap@gmail.com>
2020-10-10 17:11:04 +0900

    ADD argKwargs.py condaJupyterNotebook.txt. decorator.py

---
+++ decorator.py
@@ -0,0 +1,24 @@
+def decorator_function(func):
+
+    def inner_func():
+        print("First")
+
+        func()
+
+    return inner_func
+
+def test():
+    print("Second")
+
+test() # Second
+
+test1 = decorator_function(test)
+
+test1() # First Second   # decorator_function(test)()
+
+
+@decorator_function
+def test2():  
+    print("Third")
+
+test2() # First Third
