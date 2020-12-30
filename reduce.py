c3526ba36dfb9a3dccfd0b7205e5670a92f4d0a7
enginepdf <yeindap@gmail.com>
2020-12-30 22:59:18 +0900

    Update reduce.py
    added a comment

--- reduce.py
+++ reduce.py
@@ -10,4 +10,4 @@ def accumulator(acc, item):
 print(reduce(accumulator,(number1+number2),0)) # 633
 print(number1) # [10, 11, 12]
 
-print(reduce(lambda acc, result:acc*result,number2,2)) # 12000000
+print(reduce(lambda acc, cur:acc*cur,number2,2)) # 12000000  result:acc*result
