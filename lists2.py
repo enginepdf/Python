87195ad473de61cc4cd21ca901e983b9e8bc4c98
enginepdf <yeindap@gmail.com>
2020-12-31 14:27:06 +0900

    Update lists2.py
    index

--- lists2.py
+++ lists2.py
@@ -1,6 +1,8 @@
 items=['apple','orange','melon']
 
-print(items.index('orange',0,2)) # 1
+print(items.index('orange',0,2)) # 1  starts from index 0
+
+print(items.index('orange',2)) # ValueError: 'orange' is not in list
 
 print('melon' in items) # True
 
