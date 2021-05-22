149ff65d701d056cc3f3fb2ffad85eab48b9a823
enginepdf <yeindap@gmail.com>
2021-05-22 19:59:01 +0900

    Update argKwargs.py
    positional/keyword arguments concept

--- argKwargs.py
+++ argKwargs.py
@@ -1,10 +1,12 @@
 def my_function(*apple):
+    print(type(apple)) # <class 'tuple'>     positional arguments
     print("the newest one is "+apple[1])
 
 my_function("iphone7", 'ipad mini5')  #  the newest one is ipad mini5
 
 
 def my_function2(**apple):
+    print(type(apple)) # <class 'dict'>      keyword arguments
     print("the lastest one is "+ apple["ipad"])
 
 my_function2(iphone="iphone7", ipad="ipad mini5") # the lastest one is ipad mini5
